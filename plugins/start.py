37a231dfab579b0f98703163affb1057f76fe8bc
Bag Boy <73358379+BAGBOY555@users.noreply.github.com>
2021-08-02 13:26:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,11 +94,11 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>ഞങ്ങളുടെ Data Base പ്രകാരം നിങ്ങൾ ഇതുവരെ ഞങ്ങളുടെ Update Channel ലിൽ join ചെയ്തിട്ടില്ല\n\nഅതുകൊണ്ട് താഴെ കാണുന്ന **join Channel** എന്ന link ഉപയോഗിച്ചു update channel ലിൽ member ആയ ശേഷം</b>"
+    text = "<b>ഞങ്ങളുടെ Data Base പ്രകാരം നിങ്ങൾ ഇതുവരെ ഞങ്ങളുടെ Update Channel ലിൽ join ചെയ്തിട്ടില്ല\n\nഅതുകൊണ്ട് താഴെ കാണുന്ന **join Channel** എന്ന link ഉപയോഗിച്ചു update channel ലിൽ member ആവൂ</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>വീണ്ടും ഈ refresh button click ചെയ്ത് start കൊടുക്ക്<a href='https://t.me/{client.username}?start={argument}'>refresh</a></b>"
+        text = text + f" <b>ശേഷം വീണ്ടും ഈ refresh button click ചെയ്ത് start കൊടുക്ക്<a href='https://t.me/{client.username}?start={argument}'> refresh ചെയ്യാൻ ഈ text touch ചെയൂ.</a></b>"
     except ValueError:
         pass
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url = client.invitelink)]])
