6c853d5c76b6d15698b7710f3cea53aa4ac65b92
Irupc <68991705+Irupc@users.noreply.github.com>
2021-10-20 15:44:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -22,7 +22,7 @@ async def start_command(client: Client, message: Message):
         if len(argument) == 3:
             try:
                 ids = [int(argument[1])]
-                channel = [int(argument[2])]
+                channel = f"-100{int(argument[2])}"
             except:
                 return
         temp_msg = await message.reply("Please wait...")
@@ -58,26 +58,6 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
         return
 
 @Bot.on_message(filters.command('start') & filters.private)
