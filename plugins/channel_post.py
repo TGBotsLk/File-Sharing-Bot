ddc01c65f44ee315dad6dfd8dc05eb859dde23a7
Irupc <68991705+Irupc@users.noreply.github.com>
2021-10-20 15:51:47 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -21,8 +21,8 @@ async def channel_post(client: Client, message: Message):
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
         return
-    converted_id = post_message.message_id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
+    converted_id = post_message.message_id
+    string = f"irupc-{converted_id}-{CHANNEL_ID.split("-100")[1]}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
@@ -39,8 +39,8 @@ async def new_post(client: Client, message: Message):
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    converted_id = message.message_id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
+    converted_id = message.message_id
+    string = f"irupc-{converted_id}-{CHANNEL_ID.split("-100")[1]}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
